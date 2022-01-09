32402bdb8ad5d7bb26cc74daf887f01f5743521f
Wendel Soares <wendel.soares.dev@gmail.com>
2022-01-09 13:15:25 -0300

    Criando a class HourCOntract

---
+++ ExercicioEnumEComp/Entities/HourContract.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ExercicioEnumEComp.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        //método
+
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
+
+    }
+}
